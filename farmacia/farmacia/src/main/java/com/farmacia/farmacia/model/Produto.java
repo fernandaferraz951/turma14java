69dc292499fa460095bfb61fc9a7b5f3533315d9
Fernanda Ferraz <fernanda_ferraz2015@outlook.com>
2021-01-26 21:57:04 -0300

    Farmacia Spring Boot

---
+++ farmacia/farmacia/src/main/java/com/farmacia/farmacia/model/Produto.java
@@ -0,0 +1,65 @@
+package com.farmacia.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.sun.istack.NotNull;
+
+
+@Entity
+@Table (name = "produto")
+public class Produto {
+
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotNull
+	private String nomeProduto;
+
+	@NotNull
+	private String descricao;
+
+	@NotNull
+	private int preco;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getPreco() {
+		return preco;
+	}
+
+	public void setPreco(int preco) {
+		this.preco = preco;
+	}
+
+	
+
+}
